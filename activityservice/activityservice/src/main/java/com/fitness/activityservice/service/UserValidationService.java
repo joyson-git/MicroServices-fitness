0dbd690af118fa1806f45ebadd1c48e39f18d49c
joyson <joysonpinto77@gmail.com>
2025-04-28 23:24:37 +0530

    your update message here

--- activityservice/activityservice/src/main/java/com/fitness/activityservice/service/UserValidationService.java
+++ activityservice/activityservice/src/main/java/com/fitness/activityservice/service/UserValidationService.java
@@ -4,15 +4,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
- /*
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class UserValidationService {
- //   private final WebClient userServiceWebClient;
+   private final WebClient userServiceWebClient;
 
     public boolean validateUser(String userId) {
-        log.info("Calling User Validation API for userId: {}", userId);
+   //     log.info("Calling User Validation API for userId: {}", userId);
        try{
             return userServiceWebClient.get()
                     .uri("/api/users/{userId}/validate", userId)
@@ -29,4 +31,4 @@ public class UserValidationService {
     }
 }
 
-*/
+
